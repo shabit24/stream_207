a5cf6afb18cd9255c658882e265dc3b900e74ff7
shabit24 <hossain.shabit24@gmail.com>
2023-06-19 15:54:57 +0700

    Add project files.

---
+++ file1.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string baris;
+	ofstream outfile;
+	outfile.open("namafile.txt");
+	cout << ">= Menulis file, \'q\' untuk kelur" << endl;
+
+	while (true) {
+		cout << "- ";
+		getline(cin, baris);
+
+		if (baris == "q") break;
+		outfile << baris << endl;
+
+
+	}
+	outfile.close();
+	ifstream infile;
+	infile.open("namafile.txt");
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	if (infile.is_open()) {
+		while (getline(infile, baris)) {
+			cout << baris << '\n';
+		}
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
+}
